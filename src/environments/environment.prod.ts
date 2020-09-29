8083b2957a2f0180931d6a75cfef5d52a47a22c2
Yatharth Gupta <34278681+yath2307@users.noreply.github.com>
2020-09-29 21:57:37 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  SHOPIFY_API_KEY: '067aa25b44cf9f5ff480499137d8dadd',
+  SHOPIFY_API_SECRET_KEY: '067aa25b44cf9f5ff480499137d8dadd',
+  apiUrl: 'https://yatharth-rivia.myshopify.com',
+  authCode: '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'
 };
