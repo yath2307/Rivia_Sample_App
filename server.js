3b553d0560451dce1a8a1ae8a7b3e4b6b0a7823a
LAPTOP-8DP6GFO5\parth <yatharthgupta230799@gmail.com>
2020-09-30 00:46:14 +0530

    heroku Ready

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rivia'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/rivia/index.html'));
+var express  = require('express');
+var app      = express();                               
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
+ 
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+ 
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
